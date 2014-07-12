59fa69d67c4a718aca90661799b0ee40631e057f
Dwayne <dwza@gmx.de>
2014-07-12 21:17:54 +0200

    Versioncontrol in config

--- php/config.php
+++ php/config.php
@@ -7,13 +7,23 @@
     ini_set( "display_startup_errors", 	"1"	);
 	#--------------------------------------------------------------#
     
+    #Function and Info
+    require_once( "php/_function/function.misc.php" 	);
+    
+    define( "SMARTY_LATEST",     getLatestVersion("Smarty")          );
+    define( "SMARTY_VERSION",    getLatestVersion("Smarty", true)    );
+    define( "JQUERY_VERSION",    getLatestVersion("jQuery", true)    );
+    define( "JQUERYUI_VERSION",  getLatestVersion("jQueryUI", true)  );
+    define( "BOOTSTRAP_VERSION", getLatestVersion("Bootstrap", true) );
+    define( "TINYMCE_VERSION",   getLatestVersion("TinyMCE", true)   );
+
 	$latest_smarty_version = "3.1.19";
 
-	# Libary-phps
+	# Library-phps
 	#--------------------------------------------------------------#
-	require_once( "php/Smarty-" .$latest_smarty_version . "/libs/Smarty.class.php" 	);
+	require_once( SMARTY_LATEST . "/libs/Smarty.class.php" 	);
 	require_once( "php/_class/class.controller.php" 	);
-	require_once( "php/_function/function.misc.php" 	);
+	
 	#--------------------------------------------------------------#
 	
 	
