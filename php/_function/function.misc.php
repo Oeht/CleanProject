4da3d3768f8f7f90b3f8d43a08e1a2c63897e07a
Dwayne <dwza@gmx.de>
2014-07-12 21:18:06 +0200

    update for Versioncontrol

--- php/_function/function.misc.php
+++ php/_function/function.misc.php
@@ -1,9 +1,10 @@
 <?php
 
 	
-	function debug( $array, $detailed = false ) {
+	function debug( $array, $description="", $detailed = false ) {
 		
-		echo "<pre>";
+        
+		echo "<h1>{$description}</h1><pre>";
 		if( !$detailed ) {
 			print_r( $array );
         }
@@ -18,11 +19,38 @@
 	
     function create_log( $string ) {
     
-        $dateiname = "log.txt"; // Name der Datei
-
-        // Datei öffnen, wenn nicht vorhanden dann wird die Datei erstellt.
+        $dateiname = "log.txt";                 // Name der Datei
+                                                // Datei öffnen, wenn nicht vorhanden dann wird die Datei erstellt.
         $handler = fOpen($dateiname , "w+");
-        fWrite($handler , $string); // Dateiinhalt in die Datei schreiben
-        fClose($handler); // Datei schließen
+        fWrite($handler , $string);             // Dateiinhalt in die Datei schreiben
+        fClose($handler);                       // Datei schließen
+    }
+    
+    function getLatestVersion($type, $versionOnly = false){
+    
+        $searchString['Smarty']     = "php/Smarty-*";
+        $searchString['jQuery']     = "js/jQuery-*";
+        $searchString['jQueryUI']   = "js/jQueryUI-*";
+        $searchString['Bootstrap']  = "js/bootstrap-*";
+        $searchString['TinyMCE']    = "js/tinyMCE-*";
+       
+        $versions = glob($searchString[$type]);
+
+        $latest = array_reduce($versions, function ($latest, $folder) use ($searchString, $type, $versionOnly){
+            if (!$latest) {
+                #return $folder;
+            }
+            
+            $sub = substr($searchString[$type],0,-1);
+
+            $latestNum = preg_replace('!^' . $sub . '!', '', $latest);
+            $folderNum = preg_replace('!^' . $sub . '!', '', $folder);
+
+            if($versionOnly)
+                return version_compare($latestNum, $folderNum, '>') ? $latestNum : $folderNum;
+            else
+                return version_compare($latestNum, $folderNum, '>') ? $latest : $folder;
+        });
+        return $latest;
     }
 ?>
